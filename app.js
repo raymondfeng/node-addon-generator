0defd964fd0b895480393ff5e5aff685b3794eb8
Raymond Feng <raymond@strongloop.com>
2013-06-16 15:14:06 -0700

    Fix the templates for binding.gyp, addon, and objects

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
+var fs= require('fs');
+
 var myObj = {
     name: 'MyObject',
-        functions: [
-    {
-        name: 'add',
-        cppName: 'Add',
-        args: [
-            {type: 'Number'},
-            {type: 'Number'},
-            {type: 'Function'}
-        ],
-        return: {
-            type: 'Number'
-        }
-    },
-            {
-                name: 'multiply',
-                cppName: 'Multiply',
-                args: [
-                    {type: 'Number'},
-                    {type: 'Number'},
-                    {type: 'Function'}
-                ],
-                return: {
-                    type: 'Number'
-                },
-                async: true
+    functions: [
+        {
+            name: 'add',
+            cppName: 'Add',
+            args: [
+                {type: 'Number'},
+                {type: 'Number'},
+                {type: 'Function'}
+            ],
+            return: {
+                type: 'Number'
             }
-]
+        },
+        {
+            name: 'multiply',
+            cppName: 'Multiply',
+            args: [
+                {type: 'Number'},
+                {type: 'Number'},
+                {type: 'Function'}
+            ],
+            return: {
+                type: 'Number'
+            },
+            async: true
+        }
+    ]
 };
 var addon = {
+    name: 'MyAddon',
     objects: [
-       myObj
+        myObj
+    ], files: [
+        'src/MyObject.h',
+        'src/MyObject.cpp'
     ]
 }
 
@@ -38,21 +44,27 @@ var ejs = require('ejs');
 var path = require('path');
 
 var template = path.join(__dirname, 'templates/addon/src/object.cpp.ejs');
-ejs.renderFile(template, {obj: myObj}, function(err, content) {
-    if(!err)
-    console.log(content);
+ejs.renderFile(template, {obj: myObj}, function (err, content) {
+    if (!err)
+        console.log(content);
 });
 
 template = path.join(__dirname, 'templates/addon/src/object.h.ejs');
-ejs.renderFile(template, {obj: myObj}, function(err, content) {
-    if(!err)
+ejs.renderFile(template, {obj: myObj}, function (err, content) {
+    if (!err)
         console.log(content);
 });
 
-/*
-template = path.join(__dirname, 'templates/addon/src/addon.cpp.ejs');
-ejs.renderFile(template, {obj: myObj}, function(err, content) {
-    if(!err)
+template = path.join(__dirname, 'templates/addon/binding.gyp.ejs');
+ejs.renderFile(template, {addon: addon}, function (err, content) {
+    if (!err)
         console.log(content);
 });
-*/
+
+
+template = path.join(__dirname, 'templates/addon/src/addon.cpp.ejs');
+ejs.renderFile(template, {addon: addon}, function (err, content) {
+    if (!err)
+        console.log(content);
+});
+
