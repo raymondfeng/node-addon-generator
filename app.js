b11a42aa865c65db77ce631e2e2b318be2dba93c
Raymond Feng <raymond@strongloop.com>
2013-06-17 08:47:17 -0700

    Add more comments

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
 var fs = require('fs');
 
-var myObj = {
-    name: 'MyObject',
-    members: [
-        {name: 'a', type: 'String'},
-        {name: 'b', type: 'Boolean'},
-        {name: 'c', type: 'Object'},
-        {name: 'd', type: 'Number'}
-    ],
-    functions: [
-        {
-            name: 'add',
-            cppName: 'Add',
-            args: [
-                {name: 'x', type: 'Number'},
-                {name: 'y', type: 'Number'},
-                {name: 'callback', type: 'Function'}
-            ],
-            return: {
-                type: 'Number'
-            },
-            static: false
-        },
-        {
-            name: 'multiply',
-            cppName: 'Multiply',
-            args: [
-                {name: 'x', type: 'Number'},
-                {name: 'y', type: 'Number'},
-                {name: 'callback', type: 'Function'}
-            ],
-            return: {
-                type: 'Number'
-            },
-            async: true
-        }
-    ]
-};
 var addon = {
-    name: 'MyAddon',
-    version: '0.0.1',
+    name: 'MyAddon', // Name of the addon
+    version: '0.0.1', // Version of the addon
+
+    // An array of C++ class declarations
     objects: [
-        myObj
-    ], files: [
-        'src/MyObject.h',
-        'src/MyObject.cpp'
+        {
+            name: 'MyObject', // Name of the class
+            // An array of member declarations
+            members: [
+                {name: 'a', type: 'String'}, // Name and JS type
+                {name: 'b', type: 'Boolean'},
+                {name: 'c', type: 'Object'},
+                {name: 'd', type: 'Number'}
+            ],
+            // An array of method declarations
+            functions: [
+                {
+                    name: 'add', // JS function name
+                    cppName: 'Add', // C++ function name
+                    // An array of method arguments
+                    args: [
+                        {name: 'x', type: 'Number'},
+                        {name: 'y', type: 'Number'},
+                        {name: 'callback', type: 'Function'}
+                    ],
+                    // Return type
+                    return: {
+                        type: 'Number'
+                    },
+                    static: false
+                },
+                {
+                    name: 'multiply',
+                    cppName: 'Multiply',
+                    args: [
+                        {name: 'x', type: 'Number'},
+                        {name: 'y', type: 'Number'},
+                        {name: 'callback', type: 'Function'}
+                    ],
+                    return: {
+                        type: 'Number'
+                    },
+                    async: true // Async with callback
+                }
+            ]
+        }
     ]
 }
 
@@ -52,30 +54,4 @@ var addon = {
 var generator = require('./lib/generators/addon');
 generator.generate(addon);
 
-/*
- var template = path.join(__dirname, 'templates/addon/src/object.cpp.ejs');
- ejs.renderFile(template, myObj, function (err, content) {
- if (!err)
- console.log(content);
- });
-
- template = path.join(__dirname, 'templates/addon/src/object.h.ejs');
- ejs.renderFile(template, myObj, function (err, content) {
- if (!err)
- console.log(content);
- });
-
- template = path.join(__dirname, 'templates/addon/binding.gyp.ejs');
- ejs.renderFile(template, addon, function (err, content) {
- if (!err)
- console.log(content);
- });
-
-
- template = path.join(__dirname, 'templates/addon/src/addon.cpp.ejs');
- ejs.renderFile(template, addon, function (err, content) {
- if (!err)
- console.log(content);
- });
- */
 
